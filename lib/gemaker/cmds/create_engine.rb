6a6a59aae8a8f6f17f091352fd391640e0ceed16
Saratscheff <saratscheff@gmail.com>
2021-08-20 12:18:21 -0400

    chore: Order engine creation methods by call order

--- lib/gemaker/cmds/create_engine.rb
+++ lib/gemaker/cmds/create_engine.rb
@@ -48,6 +48,72 @@ module Gemaker
         add_rspec
       end
 
+      def add_active_admin_related
+        add_empty_directory("admin")
+        add_empty_directory("app/views/admin")
+      end
+
+      def add_app_empty_directories
+        add_empty_directory("app/decorators/#{gem_name}")
+        add_empty_directory("app/observers/#{gem_name}")
+        add_empty_directory("app/services/#{gem_name}")
+        add_empty_directory("app/commands/#{gem_name}")
+        add_empty_directory("app/values/#{gem_name}")
+      end
+
+      def add_mailer_related
+        copy_template(
+          "engine/app/mailers/application_mailer.rb",
+          "app/mailers/#{gem_name}/application_mailer.rb"
+        )
+        add_empty_directory("app/views/#{gem_name}")
+      end
+
+      def add_models_related
+        copy_template(
+          "engine/app/models/application_record.rb",
+          "app/models/#{gem_name}/application_record.rb"
+        )
+      end
+
+      def add_controllers_related
+        copy_template(
+          "engine/app/controllers/application_controller.rb",
+          "app/controllers/#{gem_name}/application_controller.rb"
+        )
+      end
+
+      def add_jobs_related
+        copy_template(
+          "engine/app/jobs/application_job.rb",
+          "app/jobs/#{gem_name}/application_job.rb"
+        )
+      end
+
+      def extend_active_admin_config
+        copy_template("engine/lib/activeadmin_config.rb", "lib/#{gem_name}/activeadmin_config.rb")
+      end
+
+      def extend_papertrail_engine_config
+        copy_template("engine/lib/papertrail_config.rb", "lib/#{gem_name}/papertrail_config.rb")
+      end
+
+      def extend_paper_trail_gem_config
+        copy_template("engine/lib/paper_trail_configuration.rb", "lib/#{gem_name}/paper_trail_configuration.rb")
+      end
+
+      def add_engine_file
+        copy_template("engine/lib/engine.rb", "lib/#{gem_name}/engine.rb")
+      end
+
+      def add_errors_file
+        copy_template("engine/lib/errors.rb", "lib/#{gem_name}/errors.rb")
+      end
+
+      def add_main_file
+        copy_template("engine/lib/main.rb", "lib/#{gem_name}.rb")
+      end
+
       def add_git_ignore
         copy_file("engine/.gitignore", ".gitignore")
       end
@@ -66,72 +132,6 @@ module Gemaker
 
       def add_rspec
         copy_file("engine/.rspec", ".rspec")
-      end
-
-      def extend_active_admin_config
-        copy_template("engine/lib/activeadmin_config.rb", "lib/#{gem_name}/activeadmin_config.rb")
-      end
-
-      def add_engine_file
-        copy_template("engine/lib/engine.rb", "lib/#{gem_name}/engine.rb")
-      end
-
-      def add_errors_file
-        copy_template("engine/lib/errors.rb", "lib/#{gem_name}/errors.rb")
-      end
-
-      def extend_paper_trail_gem_config
-        copy_template("engine/lib/paper_trail_configuration.rb", "lib/#{gem_name}/paper_trail_configuration.rb")
-      end
-
-      def extend_papertrail_engine_config
-        copy_template("engine/lib/papertrail_config.rb", "lib/#{gem_name}/papertrail_config.rb")
-      end
-
-      def add_main_file
-        copy_template("engine/lib/main.rb", "lib/#{gem_name}.rb")
-      end
-
-      def add_controllers_related
-        copy_template(
-          "engine/app/controllers/application_controller.rb",
-          "app/controllers/#{gem_name}/application_controller.rb"
-        )
-      end
-
-      def add_jobs_related
-        copy_template(
-          "engine/app/jobs/application_job.rb",
-          "app/jobs/#{gem_name}/application_job.rb"
-        )
-      end
-
-      def add_models_related
-        copy_template(
-          "engine/app/models/application_record.rb",
-          "app/models/#{gem_name}/application_record.rb"
-        )
-      end
-
-      def add_mailer_related
-        copy_template(
-          "engine/app/mailers/application_mailer.rb",
-          "app/mailers/#{gem_name}/application_mailer.rb"
-        )
-        add_empty_directory("app/views/#{gem_name}")
-      end
-
-      def add_active_admin_related
-        add_empty_directory("admin")
-        add_empty_directory("app/views/admin")
-      end
-
-      def add_app_empty_directories
-        add_empty_directory("app/decorators/#{gem_name}")
-        add_empty_directory("app/observers/#{gem_name}")
-        add_empty_directory("app/services/#{gem_name}")
-        add_empty_directory("app/commands/#{gem_name}")
-        add_empty_directory("app/values/#{gem_name}")
       end
     end
   end
